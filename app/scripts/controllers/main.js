4ade89e7c2e03ba58244f5a8d8239c12efc0648a
杨灯 <yangdeng@haizhi.com>
2016-04-18 17:50:12 +0800

    new project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smallDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smallDemoApp
+ */
+angular.module('smallDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
